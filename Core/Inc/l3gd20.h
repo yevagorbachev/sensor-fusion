31ab605443e6d13f9c0a1689e9f2c6b608490307
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2022-08-20 09:47:02 -0400

    init from stm32 project files

---
+++ Core/Inc/l3gd20.h
@@ -0,0 +1,15 @@
+/*
+ * l3gd20.h
+ *
+ *  Created on: Aug 17, 2022
+ *      Author: Yevgeniy Gorbachev
+ */
+
+#ifndef INC_L3GD20_H_
+#define INC_L3GD20_H_
+
+
+
+
+
+#endif /* INC_L3GD20_H_ */
---
+++ Core/Inc/lsm303dlhc.h
@@ -0,0 +1,18 @@
+/*
+ * ldm303dlhc.h
+ *
+ *  Created on: Aug 17, 2022
+*      Author: Yevgeniy Gorbachev
+ */
+
+#ifndef INC_LSM303DLHC_H_
+#define INC_LSM303DLHC_H_
+
+
+#define LSM303_A_CTL(index) 0x19 + (index)
+#define LSM303_A_REF 0x26
+#define LSM303_A_STATUS 0x27
+#define LSM303_A_DATA 0x28
+
+
+#endif /* INC_LSM303DLHC_H_ */
